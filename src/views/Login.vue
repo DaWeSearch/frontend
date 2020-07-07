f25cf363da3238d8566274dd6cef6241364b0c88
Mathias <mathias.kuhn@web.de>
2020-07-07 14:40:39 +0200

    test implementation static wordcloud

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,10 +9,10 @@
         </b-nav>
 
         <div class="mt-5 d-flex">
-            <b-card 
+            <b-card
             class="mx-auto"
             header="Log in"
-            header-tag="header" 
+            header-tag="header"
             footer-tag="footer"
             >
                 <b-form @submit="onSubmit">
@@ -26,20 +26,57 @@
                 </template>
             </b-card>
         </div>
+        <!-- erstes beispiel-->
+        <div>
+            <cloud :data="words"
+                   :fontSizeMapper="fontSizeMapper" />
+        </div>
+        <!-- zweites bsp -->
+        <wordcloud
+                :data="defaultWords"
+                nameKey="name"
+                valueKey="value"
+                :color="myColors"
+                :showTooltip="true"
+                :wordClick="wordClickHandler">
+        </wordcloud>
 
     </div>
 </template>
 
 <script>
 import SessionStore from "../stores/SessionStore"
+//import * as d3 from "d3";
+// https://www.npmjs.com/package/vue-d3-cloud
+// https://github.com/d3/d3/wiki
+import Cloud from 'vue-d3-cloud'
+import wordcloud from 'vue-wordcloud'
 
 export default {
     name: 'Login',
     data: () => {
         return {
             username: "",
-            password: ""
-        };
+            password: "",
+            myColors: ['#000000', '#0000000', '#0000000', '#0000000'],
+            defaultWords: [
+                {"name": "Cat","value": 26},
+                {"name": "fish","value": 19},
+                {"name": "things","value": 18},
+                {"name": "look","value": 16},
+                {"name": "two","value": 15},
+            ],
+            words: [
+                { text: 'Bitcoin', value: 1000 },
+                { text: 'Ethereum', value: 200 },
+                { text: 'blockchain', value: 800 },
+                { text: 'very cool', value: 1000000 },
+                { text: 'dawesys', value: 100 },
+            ],
+
+            //größe der wörter beispiel 1
+            fontSizeMapper: word => Math.log2(word.value) * 5,
+        }
     },
     methods: {
         onSubmit(evt){
@@ -48,9 +85,16 @@ export default {
             .then(data => {console.log("hier ist ein signupresponse");SessionStore.data.authKey=data})
             .catch(error => console.log(error));
         },
+        wordClickHandler(name, value, vm) {
+            console.log('wordClickHandler', name, value, vm);
+        },
         printAuthKey(){
             console.log(SessionStore.data.authKey)
         }
+    },
+    components: {
+        Cloud,
+        wordcloud,
     },
 }
 </script>
