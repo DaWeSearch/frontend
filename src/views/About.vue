4c0e2fa7560c9e28c91ab839a3ad9f3d1df1fcf0
Peter Abdou <abdou@campus.tu-berlin.de>
2020-07-13 20:47:43 +0200

    About page Begin

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="about">
     <b-nav tabs >
-      <b-nav-item><router-link to="/">Reviews</router-link></b-nav-item>
+      <b-nav-item disabled><router-link to="/">Reviews</router-link></b-nav-item>
       <b-nav-item disabled><router-link to="/search">Search</router-link></b-nav-item>
       <b-nav-item disabled> <router-link to="/score">Score</router-link></b-nav-item>
       <b-nav-item active><router-link to="/about">About</router-link></b-nav-item>
+      <b-nav-item><router-link to="/login">Login</router-link></b-nav-item>
     </b-nav>
-    <h1>You should 
-      <b-nav-item><router-link to="/login">log in</router-link></b-nav-item></h1>
+    <b-jumbotron header="DaWeSearch" lead="A tool accelerating your structured literature review">
+      <b-button variant="primary" href="/login">Get started!</b-button>
+    </b-jumbotron>
+    <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/watch?v=2NmDlsy_Igw"
+            allowfullscreen
+    ></b-embed>
+
+
+
 
   </div>
 </template>
